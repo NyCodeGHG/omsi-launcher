fa740dd4627c1f6596fa8fcc39a6ae952b5134ba
marie <me@nycode.dev>
2022-05-15 18:38:48 +0200

    Run ktlint

--- src/main/kotlin/ui/instance/creation/InstanceCreationDialog.kt
+++ src/main/kotlin/ui/instance/creation/InstanceCreationDialog.kt
@@ -45,138 +45,139 @@ fun InstanceCreationDialog(
                 onCloseRequest()
                 createInstance(instanceCreationState)
             }, modifier = Modifier.align(Alignment.BottomCenter), enabled = isValid()) {
-                Text(strings.createInstance)
+                    Text(strings.createInstance)
+                }
+                }
             }
         }
-    }
-}
 
-@Composable
-private fun InstanceCreationForm(
-    instanceCreationState: InstanceCreationState,
-) = with(instanceCreationState) {
-    val strings = LocalStrings.current
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = strings.createANewInstance,
-            style = MaterialTheme.typography.h3,
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-        )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Column(
-                modifier = Modifier.padding(top = 8.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                OutlinedTextField(
-                    value = name,
-                    onValueChange = { name = it },
-                    placeholder = {
-                        Text(strings.newInstance)
-                    },
-                    label = {
-                        Text(strings.instanceName)
-                    },
-                    singleLine = true
+        @Composable
+        private fun InstanceCreationForm(
+            instanceCreationState: InstanceCreationState,
+        ) = with(instanceCreationState) {
+            val strings = LocalStrings.current
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = strings.createANewInstance,
+                    style = MaterialTheme.typography.h3,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
                 )
-                Spacer(modifier = Modifier.height(8.dp))
-                PathInputField(
-                    value = path,
-                    onValueChange = { path = it },
-                    label = {
-                        Text(strings.instanceDirectory)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(top = 8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { name = it },
+                            placeholder = {
+                                Text(strings.newInstance)
+                            },
+                            label = {
+                                Text(strings.instanceName)
+                            },
+                            singleLine = true
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        PathInputField(
+                            value = path,
+                            onValueChange = { path = it },
+                            label = {
+                                Text(strings.instanceDirectory)
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        DropDownColumn(
+                            title = strings.patchVersion,
+                            strings = strings,
+                            value = patchVersion,
+                            onValueChange = { patchVersion = it },
+                            values = Instance.PatchVersion.VALUES
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        DropDownColumn(
+                            title = strings.logLevel,
+                            strings = strings,
+                            value = logLevel,
+                            onValueChange = { logLevel = it },
+                            values = InstanceOptions.LogLevel.VALUES
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        DropDownColumn(
+                            title = strings.screenMode,
+                            strings = strings,
+                            value = screenMode,
+                            onValueChange = { screenMode = it },
+                            values = InstanceOptions.ScreenMode.VALUES
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
                     }
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                DropDownColumn(
-                    title = strings.patchVersion,
-                    strings = strings,
-                    value = patchVersion,
-                    onValueChange = { patchVersion = it },
-                    values = Instance.PatchVersion.VALUES
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                DropDownColumn(
-                    title = strings.logLevel,
-                    strings = strings,
-                    value = logLevel,
-                    onValueChange = { logLevel = it },
-                    values = InstanceOptions.LogLevel.VALUES
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                DropDownColumn(
-                    title = strings.screenMode,
-                    strings = strings,
-                    value = screenMode,
-                    onValueChange = { screenMode = it },
-                    values = InstanceOptions.ScreenMode.VALUES
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-            Column(Modifier.padding(top = 8.dp)) {
-                CheckboxRow(
-                    title = strings.use4gbPatch,
-                    tooltip = strings.use4gbPatchTooltip,
-                    value = use4gbPatch
-                ) { use4gbPatch = it }
-                CheckboxRow(
-                    title = strings.saveLogs,
-                    tooltip = strings.saveLogsTooltip(path?.absolutePathString()),
-                    value = saveLogs
-                ) { saveLogs = it }
-                CheckboxRow(
-                    title = strings.debugMode,
-                    tooltip = strings.debugModeTooltip,
-                    value = useDebugMode
-                ) { useDebugMode = it }
+                    Column(Modifier.padding(top = 8.dp)) {
+                        CheckboxRow(
+                            title = strings.use4gbPatch,
+                            tooltip = strings.use4gbPatchTooltip,
+                            value = use4gbPatch
+                        ) { use4gbPatch = it }
+                        CheckboxRow(
+                            title = strings.saveLogs,
+                            tooltip = strings.saveLogsTooltip(path?.absolutePathString()),
+                            value = saveLogs
+                        ) { saveLogs = it }
+                        CheckboxRow(
+                            title = strings.debugMode,
+                            tooltip = strings.debugModeTooltip,
+                            value = useDebugMode
+                        ) { useDebugMode = it }
+                    }
+                }
             }
         }
-    }
-}
 
-@Composable
-private fun <E : Translatable> DropDownColumn(
-    title: String,
-    strings: Strings,
-    value: E,
-    onValueChange: (E) -> Unit,
-    values: Collection<E>,
-) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            title,
-            fontSize = 20.sp,
-            modifier = Modifier
-                .align(Alignment.Start)
-                .padding(start = 8.dp)
-        )
-        Spacer(Modifier.height(8.dp))
-        DropdownInputField(
-            value = value,
-            onValueChange = onValueChange,
-            modifier = Modifier.width(280.dp),
-            values = values
+        @Composable
+        private fun <E : Translatable> DropDownColumn(
+            title: String,
+            strings: Strings,
+            value: E,
+            onValueChange: (E) -> Unit,
+            values: Collection<E>,
         ) {
-            Text(it.translation(strings))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    title,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(start = 8.dp)
+                )
+                Spacer(Modifier.height(8.dp))
+                DropdownInputField(
+                    value = value,
+                    onValueChange = onValueChange,
+                    modifier = Modifier.width(280.dp),
+                    values = values
+                ) {
+                    Text(it.translation(strings))
+                }
+            }
         }
-    }
-}
 
-@Composable
-private fun CheckboxRow(
-    title: String,
-    tooltip: String,
-    value: Boolean,
-    onValueChange: (Boolean) -> Unit,
-) {
-    TooltipWrapper(tooltip = { TooltipText(tooltip) }) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(value, onValueChange)
-            Text(title)
+        @Composable
+        private fun CheckboxRow(
+            title: String,
+            tooltip: String,
+            value: Boolean,
+            onValueChange: (Boolean) -> Unit,
+        ) {
+            TooltipWrapper(tooltip = { TooltipText(tooltip) }) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(value, onValueChange)
+                    Text(title)
+                }
+            }
         }
-    }
-}
+        
